88cddfcb0435d6d2245571699e93ecd3299faa71
Mystic Weng <710120644@qq.com>
2020-06-08 22:10:02 +0800

    by pass

--- user/src/main/java/com/canteen/user/UserApplication.java
+++ user/src/main/java/com/canteen/user/UserApplication.java
@@ -3,7 +3,7 @@ package com.canteen.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class UserApplication {
 
 	public static void main(String[] args) {
