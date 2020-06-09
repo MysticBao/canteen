c2f2fb3fb65d01c6737958488ec3657e1ed48c7f
Mystic Weng <710120644@qq.com>
2020-06-09 19:46:13 +0800

    import used method

--- user/src/main/java/com/canteen/user/UserApplication.java
+++ user/src/main/java/com/canteen/user/UserApplication.java
@@ -2,6 +2,8 @@ package com.canteen.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class UserApplication {
